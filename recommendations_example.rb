8cb5329f10f525a227975ddb8caba34bdf383861
Javad Karabi <karabijavad@gmail.com>
2014-05-13 19:26:03 -0500

    more

--- recommendations_example.rb
+++ recommendations_example.rb
@@ -5,15 +5,40 @@ require 'active_support/core_ext'
 
 class RecommendationsExample < Sinatra::Base
 
-  post '/person/' do
+  post '/person/:id' do
     g = Cadet::Session.open "#{ENV['RACK_ENV']}_graph.db/"
     g.transaction do
-      person = g.Person_by_name(params["name"])
+      person = g.Person_by_id(params["id"].to_i)
       person.data = {
-        age: params["age"].to_i
+        name: params['name'],
+        age:  params['age'].to_i,
+        id:   params['id'].to_i
       }
     end
     g.close
   end
+
+  post '/interest/:id' do
+    g = Cadet::Session.open "#{ENV['RACK_ENV']}_graph.db/"
+    g.transaction do
+      interest = g.Interest_by_id(params["id"].to_i)
+      interest.data = {
+        name: params['name'],
+        id:   params['id'].to_i
+      }
+    end
+    g.close
+  end
+
+  post '/create_interest/' do
+    g = Cadet::Session.open "#{ENV['RACK_ENV']}_graph.db/"
+    g.transaction do
+      interest = g.Interest_by_id(params["interest_id"].to_i)
+      person   = g.Person_by_id(params["person_id"].to_i)
+      person.outgoing(:likes) << interest
+    end
+    g.close
+  end
+
 
 end
--- spec/recommendations_example_spec.rb
+++ spec/recommendations_example_spec.rb
@@ -1,13 +1,51 @@
 require 'spec_helper.rb'
 
+`rm -rf "#{ENV['RACK_ENV']}_graph.db/"`
+
 describe "RecommendationsExample" do
   it "should create or update a nodes attrs on POST" do
-    post '/person/', {name: "Javad", age: 25}
+
+    post '/person/10', {name: "Javad", age: 25}
 
     Cadet::Session.open "#{ENV['RACK_ENV']}_graph.db/" do
       transaction do
-        Person_by_name("Javad")[:age].should == 25
+        Person_by_id(10).data.should == {
+          name: "Javad",
+          age:  25,
+          id:   10
+        }
       end
     end
   end
+
+  it "should create or update a nodes attrs on POST" do
+
+    post '/interest/110', {name: "Pizza"}
+
+    Cadet::Session.open "#{ENV['RACK_ENV']}_graph.db/" do
+      transaction do
+        Interest_by_id(110).data.should == {
+          name: "Pizza",
+          id:   110
+        }
+      end
+    end
+  end
+
+  it "should create or update a nodes attrs on POST" do
+
+    post '/person/10',    {name: "Javad", age: 25}
+    post '/person/11',    {name: "Ellen", age: 25}
+    post '/interest/110', {name: "Pizza"}
+    post '/create_interest/', {person_id: 10, interest_id: 110}
+    post '/create_interest/', {person_id: 11, interest_id: 110}
+
+    Cadet::Session.open "#{ENV['RACK_ENV']}_graph.db/" do
+      transaction do
+        Person_by_id(10).outgoing(:likes).incoming(:likes).to_a.should == [Person_by_id(11), Person_by_id(10)]
+      end
+    end
+  end
+
+
 end
